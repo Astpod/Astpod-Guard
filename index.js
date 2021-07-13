ee1af7c983dcc9452b54f35bce6c2eb73e61445c
Astpod <49484285+Astpod@users.noreply.github.com>
2021-07-13 08:56:12 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+#Sunucunuzu bu guardla koruyabilirsiniz.
+
+# %100 etkili olucaktır. stara basarsanız database botu gelir.
+
+
+
+# Elhamdulillâhi rabbil'alemin, Errahmânir'rahim
+
+# Mâliki yevmiddin, İyyâke na'budu Ve iyyâke neste'în
+
+# İhdinessirâtal mustakîm, Sirâtallezine en'amte aleyhim Ğayrilmağdûbi aleyhim ve leddâllîn.
+
+# Kul e'ûzü birabbinnâs
+
+# Melikinnâs
+
+# İlâhinnâs
+
+# Min şerrilvesvâsilhannâs
+
+# Ellezî yüvesvisü fî sudûrinnâsi
+
+# Minelcinneti vennâs
+
+
+# Kul e'ûzü birabbil felak
+
+# Min şerri mâ halak
+
+# Ve min şerri ğasikın izâ vekab
+
+# Ve min şerrinneffâsâti fil'ukad
+
+# Ve min şerri hâsidin izâ hased
+
+
+# Bismillahirrahmânirrahîm.
+
+# Allahû lâ ilâhe illâ hûvel hayyûl kâyyum,
+  
+# lâ te'huzûhu sinetûv velâ nevm,
+
+# lehu mâ fis semâvati ve mâ fil ârd,
+  
+# men zellezi yeşfeu indehu illâ bi iznih,
+  
+# ya'lemû mâ beyne eydihim ve mâ halfehûm,
+
+# ve lâ yûhiytune bi şey'im min ilmihi illâ bi mâ şa',
+  
+# vesiâ kûrsiyyûhûs semavati vel ârd,
+  
+# ve lâ yeudûhu hifzuhûma ve hûvel aliyyûl azim.
